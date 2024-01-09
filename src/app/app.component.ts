5070c90cccf5ceb25a6fe94ab4a15fb517448695
Andrea C <andreacaicedo.ccs@gmail.com>
2024-01-08 23:09:38 -0500

    Format list of houses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HouseComponent } from './house/house.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HouseComponent],
+  imports: [CommonModule, RouterOutlet, HouseComponent, HousingLocationComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
